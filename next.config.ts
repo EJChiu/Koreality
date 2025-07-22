ff514d95b245c6c608afb88ae953b5866075b32c
EJChiu <twirlpearl1995@gmail.com>
2025-07-22 13:20:57 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 完全忽略 ESLint 錯誤
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 完全忽略 TypeScript 錯誤
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
